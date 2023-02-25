f37ed38b5ba97883e37277cbaaa6b9a23357dd42
KKbeckang <beckang@bu.edu>
2023-02-24 22:52:37 -0500

    Update the test /Create Deliverable2
    Create Deliverable2

---
+++ Deliverable2.md
@@ -0,0 +1,7 @@
+# Deliverables 2:
+
+I believe it is important to know requests python library, since it help us request from designated API URLs.
+To make this task easy, you need to first understand request libarary, then undertsand jSON format so that it is
+easy to partition the data and list them as you want. Then familiar with workflow about circle ci or Tavisci will
+Speed up the CI setup Process, There are limits when when access the github api, thats one thing need to note, Moreover,
+Setting up the test cases with proper methodology is needed.
--- test_hw04a.py
+++ test_hw04a.py
@@ -2,13 +2,15 @@ import unittest
 from hw04a import GithubRepoInfo
 
 class TestHW4(unittest.TestCase):
+    
     def testGitUser(self):
         self.assertEqual(GithubRepoInfo("KKbeckang")[0],'Repo: avi-clothing Number of commits: 8','Fail')
-
     def testNonString(self):
         self.assertEqual(GithubRepoInfo(21312412312),'The User ID should be a String','Fail')
     def testNonUser(self):
         self.assertEqual(GithubRepoInfo('esdsadasesxxzcxzczsdae232o'),'The Input User is Invalid','Fail')
+    def testEmptyString(self):
+        self.assertEqual(GithubRepoInfo(''),'"The Input cannot be empty"','Fail')
 
 
 
