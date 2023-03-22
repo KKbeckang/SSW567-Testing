f3b2e65aaa348befe76247c3ebe240a699bc8f79
KKbeckang <beckang@bu.edu>
2023-03-21 23:02:47 -0400

    Update with Mock_Test

--- hw04a.py
+++ hw04a.py
@@ -2,13 +2,17 @@ import requests
 import json
 
 def GithubRepoInfo(UserId):
+    
+    access_token = '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'
     if not isinstance(UserId, str):
         return "The User ID should be a String"
     if UserId == '':
         return "The Input cannot be empty"
     FullURL = "https://api.github.com/users/"+ UserId +"/repos"
-    read = requests.get(FullURL)
+    read = requests.get(FullURL,
+                        headers={'Authorization': f'token {access_token}'})
     record = read.json()
+    
     if not isinstance(record,list):
         return "The Input User is Invalid"
     RepoInfo=[]
@@ -17,7 +21,8 @@ def GithubRepoInfo(UserId):
     RepoURL = "https://api.github.com/repos/"
     for i in range(len(record)):
         RepoURL = "https://api.github.com/repos/" +UserId +"/"+record[i]['name']+"/commits"
-        RepoCommit = requests.get(RepoURL)
+        RepoCommit = requests.get(RepoURL,
+                        headers={'Authorization': f'token {access_token}'})
         RepoCommit = RepoCommit.json()
         RepoNames.append(record[i]['name'])
         RepoCommitsNum.append(len(RepoCommit))
@@ -26,7 +31,3 @@ def GithubRepoInfo(UserId):
     }
     return RepoInfo
 
-print(GithubRepoInfo('KKbeckang'))
-
-
-    
--- test_hw04a.py
+++ test_hw04a.py
@@ -1,18 +1,42 @@
 import unittest
+from unittest import mock
+from unittest.mock import Mock,patch,MagicMock
+
 from hw04a import GithubRepoInfo
 
-class TestHW4(unittest.TestCase):
 
-    def testGitUser(self):
+class TestHW4(unittest.TestCase):
+    """mock the result of GIthubrepo info"""
+    @mock.patch('hw04a.GithubRepoInfo')
+    def testGitUser(self,mock_GithubRepoInfo):
+        """mock our hw04a.py first see if it work"""
+        mock_GithubRepoInfo.return_value = ['Repo: avi-clothing Number of commits: 8', 'Repo: Boston_HackRan Number of commits: 6', 'Repo: Covid19_repo Number of commits: 3', 'Repo: crwn-clothing-v2 Number of commits: 5', 'Repo: CS-555-project Number of commits: 28', 'Repo: CS391-Web-Audit Number of commits: 5', 'Repo: HW02a_TestingLagacyProgram Number of commits: 10', 'Repo: KK-CS412 Number of commits: 6', 'Repo: KKbeckang Number of commits: 13', 'Repo: KKbeckang.github.io Number of commits: 1', 'Repo: Monster-Contact-Book Number of commits: 5', 'Repo: project_hoobank Number of commits: 9', 'Repo: RentPipe Number of commits: 30', 'Repo: Rentpiplib Number of commits: 2', 'Repo: SSW567-Testing Number of commits: 12', 'Repo: VOICEDATA Number of commits: 2']
         self.assertEqual(GithubRepoInfo("KKbeckang")[0],'Repo: avi-clothing Number of commits: 8','Fail')
-    def testNonString(self):
-        self.assertEqual(GithubRepoInfo(21312412312),'The User ID should be a String','Fail')
-    def testNonUser(self):
-        self.assertEqual(GithubRepoInfo('esdsadasesxxzcxzczsdae232o'),'The Input User is Invalid','Fail')
+
+    @mock.patch('requests.get')
+    def mock_Test_API_get1(self,mock_Request):
+        """example 1"""
+        mock_Request.return_value = Mock(status_code = 200, text = '[{"name":"Rentpip"}, {"name": "Jacksun"}]')
+        self.assertEqual(len(GithubRepoInfo("KKbeckang")),2)
+        self.assertEqual(GithubRepoInfo("KKbeckang"), ['Complexity','Jacksun'])
+
+    @mock.patch('requests.get')
+    def mock_Test_API_get2(self,mock_Request):
+        """example 2"""
+        mock_Request.return_value = Mock(status_code = 200, text = '[{"name":"Rentpip"}, {"name": "Jacksun"}.{"name": "Jacksun1"}.{"name": "Jacksun2"}{"name": "Jacksun3"}]')
+        self.assertEqual(len(GithubRepoInfo("KKbeckang")),5)
+        self.assertEqual(GithubRepoInfo("KKbeckang"), ['Complexity','Jacksun','Jacksun1','Jacksun2','Jacksun3'])
+
+    @mock.patch('requests.get')
+    def mock_Test_API_Not_Found(self,mock_Request):
+        """cannot be found message"""
+        mock_Request.return_value = Mock(status_code = 200, text = {'message': 'Not Found', 'documentation_url': 'https://docs.github.com/rest/reference/repos#list-repositories-for-a-user'})
+        self.assertEqual(GithubRepoInfo('esdsadasesxxzcxzczsdae232o'),'The Input User is Invalid',)
+   
     def testEmptyString(self):
         self.assertEqual(GithubRepoInfo(''),'The Input cannot be empty','Fail')
 
 
 
 if __name__ == '__main__':
-    unittest.main(exit=False)
+    unittest.main(exit=False) 
